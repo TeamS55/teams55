ce893c03f0945472c808b16f7aaa91d8d0dcfe79
Haileoshu <haileamlakgoshu25@gmail.com>
2024-05-20 15:36:45 +0200

    day1

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to Team S55</h1>
-        <h1>Website under Construction</h1>
+        <h1>Welcome to Team S55!</h1>
+        <h1>Website under Construction:)</h1>
+        <h5>brick by brick</h5>
         <p>
           Edit <code>src/App.js</code> and save to reload.
           
